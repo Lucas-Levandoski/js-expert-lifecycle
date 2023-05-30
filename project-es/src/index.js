b39b26cfa7f9e59de4fb14034763b8434702ad5c
Lucas Levandoski <lucas-levandoski@hotmail.com>
2023-05-29 21:00:49 -0300

    terminal setup

--- project-es/src/index.js
+++ project-es/src/index.js
@@ -1,3 +1,27 @@
+import Draftlog from 'draftlog';
+import chalk from 'chalk';
+import chalkTable from 'chalk-table';
+import readline from 'readline';
+
 import database from '../database.json' assert { type: 'json' };
 
-console.log('database', database);
+Draftlog(console).addLineListener(process.stdin);
+
+const options = {
+  leftPad: 2,
+  columns: [
+    { field: 'id', name: chalk.cyan("ID")},
+    { field: 'vehicles', name: chalk.magentaBright("Vehicles")},
+    { field: 'kmTraveled', name: chalk.greenBright("Km Traveled")},
+    { field: 'from', name: chalk.magenta("From")},
+    { field: 'to', name: chalk.blue("To")},
+  ]
+}
+;
+const table = chalkTable(options, database);
+const print = console.draft(table);
+
+const terminal = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
