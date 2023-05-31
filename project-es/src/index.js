0646d29522ecb8a1a863b510f2076f275553a9c1
Lucas Levandoski <lucas-levandoski@hotmail.com>
2023-05-30 22:48:53 -0300

    refactoring file structure

--- project-es/src/index.js
+++ project-es/src/index.js
@@ -1,31 +1,5 @@
-import Draftlog from 'draftlog';
-import chalk from 'chalk';
-import chalkTable from 'chalk-table';
-import readline from 'readline';
-
 import database from '../database.json' assert { type: 'json' };
+import { TerminalController } from './terminalController.js';
 
-import { Person } from './person.js';
-
-Draftlog(console).addLineListener(process.stdin);
-
-
-
-const options = {
-  leftPad: 2,
-  columns: [
-    { field: 'id', name: chalk.cyan("ID")},
-    { field: 'vehicles', name: chalk.magentaBright("Vehicles")},
-    { field: 'kmTraveled', name: chalk.greenBright("Km Traveled")},
-    { field: 'from', name: chalk.magenta("From")},
-    { field: 'to', name: chalk.blue("To")},
-  ]
-}
-;
-const table = chalkTable(options, database.map(item => new Person(item).formatted('pt')));
-const print = console.draft(table);
-
-const terminal = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-})
+const terminalController = new TerminalController();
+terminalController.initializeTerminal(database, 'pt-BR')
